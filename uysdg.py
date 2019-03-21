6b2dacf81b6056138b61aa0741b553db89c1ea2a
miri-lipp <45341225+miri-lipp@users.noreply.github.com>
2019-03-22 00:23:54 +0300

    Add files via upload

--- uysdg.py
+++ uysdg.py
@@ -1,8 +1,14 @@
-a = [0, 0, 1]
-s = int(input("Input size:"))
-k, j, i = a
-print("Tribonacci numbers:")
-for m in range(3, s):
-    a.append(i + k + j)
-    k, j, i = j, i, i + k + j
-print(a)
+def gen(s):
+    k, j, i = 0, 0, 1
+    for m in range(3, s):
+        k, j, i = j, i, i + k + j
+        yield i + j + k
+
+
+if __name__ == '__main__':
+    m = [0, 0, 1]
+    s = int(input("Input size:"))
+    for a in gen(s):
+        m.append(a)
+    print(m)
+
